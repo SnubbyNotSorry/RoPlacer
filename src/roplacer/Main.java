89f8d3212f35dec7b25144a884e4d0b31ccdf4b4
SnubbyNotSorry <macelpix@gmail.com>
2019-12-23 18:59:55 +0100

    Code can now detect Roblox and acquire the path to its current directory.

--- src/roplacer/Main.java
+++ src/roplacer/Main.java
@@ -10,66 +10,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Path path = new Path();
+        path.pathFinder();
+        path.getPath();
     }
     
-    //path that will be used to access Roblox content
-    private String path;
-    
-    public void main() {
-        pathFinder();
-        System.out.println(path);
-    }
-    
-    /**
-     * method called to find the used Roblox path folder from the running Roblox
-     * process
-     * 
-     * @return whether the path has been found (is Roblox open ?)
-     */
-    public boolean pathFinder() {
-        String oldPath = path;
-        ProcessHandle.allProcesses()
-            .forEach(process -> checkProcess(process.info()));
-        return (!oldPath.equals(path));
-    }
-    
-    /**
-     * method pathFinder() calls upon for each process to check if it is Roblox.
-     * 
-     * @param process fed by pathFinder() above
-     */
-    public void checkProcess(ProcessHandle.Info process) {
-        if (process.toString().startsWith("Roblox Game Client")) {
-            path = process.commandLine().toString();
-        }
-    }
-    
-    public String getPath() {
-        return path;
-    }
- 
-    
-    
-    
-    
-    /*
-    public static void main(String[] args) {
-    ProcessHandle.allProcesses()
-            .forEach(process -> System.out.println(processDetails(process)));
-}
-
-private static String processDetails(ProcessHandle process) {
-    return String.format("%8d %8s %10s %26s %-40s",
-            process.pid(),
-            text(process.parent().map(ProcessHandle::pid)),
-            text(process.info().user()),
-            text(process.info().startInstant()),
-            text(process.info().commandLine()));
-}
-
-private static String text(Optional<?> optional) {
-    return optional.map(Object::toString).orElse("-");
-}
-    */
 }
--- src/roplacer/Path.java
+++ src/roplacer/Path.java
@@ -1,22 +1,30 @@
+package roplacer;
+
 /**
  *
  * @author Snubby
  */
 public class Path {
-//path that will be used to access Roblox content
+    
+    //path that will be used to access Roblox content
     private String path;
+    
+    public Path() {
+    }
     
     /**
      * method called to find the used Roblox path folder from the running Roblox
      * process
      * 
-     * @return whether the path has been found (is Roblox open ?)
+     * @return whether the path has been found (is Roblox open ?). Logic behind
+     * it is that if the path finishes different than how it started, then it
+     * has been found. Will be useful later on for when Roblox updates.
      */
     public boolean pathFinder() {
         String oldPath = path;
         ProcessHandle.allProcesses()
             .forEach(process -> checkProcess(process.info()));
-        return (!oldPath.equals(path));
+        return (oldPath != path);
     }
     
     /**
@@ -25,33 +33,14 @@ public class Path {
      * @param process fed by pathFinder() above
      */
     public void checkProcess(ProcessHandle.Info process) {
-        if (process.toString().startsWith("Roblox Game Client")) {
-            path = process.commandLine().toString();
+        if (process.command().toString().endsWith("RobloxPlayerBeta.exe]")) {
+            //Roblox has been found, set path.
+            path = process.command().toString();
         }
     }
     
     public String getPath() {
         return path;
     }
- 
     
-    /*
-    public static void main(String[] args) {
-    ProcessHandle.allProcesses()
-            .forEach(process -> System.out.println(processDetails(process)));
-}
-
-private static String processDetails(ProcessHandle process) {
-    return String.format("%8d %8s %10s %26s %-40s",
-            process.pid(),
-            text(process.parent().map(ProcessHandle::pid)),
-            text(process.info().user()),
-            text(process.info().startInstant()),
-            text(process.info().commandLine()));
-}
-
-private static String text(Optional<?> optional) {
-    return optional.map(Object::toString).orElse("-");
-}
-    */
 }
